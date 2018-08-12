788102b99761731dca53cd181d44d5278fd69e0b
sherchel <997078856@qq.com>
2018-08-12 14:41:28 +0800

    v1.0

--- User/AP_main.c
+++ User/AP_main.c
@@ -1,17 +1,10 @@
 /**
   ******************************************************************************
   * @file    main.c
-  * @author  fire
-  * @version V1.0
-  * @date    2015-01-xx
-  * @brief   WF-ESP8266 WiFi模块测试
-  ******************************************************************************
-  * @attention
-  *
-  * 实验平台:野火 iSO STM32 开发板 
-  * 论坛    :http://www.firebbs.cn
-  * 淘宝    :https://fire-stm32.taobao.com
-  *
+  * @author
+  * @version 
+  * @date    
+  * @brief  
   ******************************************************************************
   */ 
  
@@ -33,13 +26,11 @@
 int main ( void )
 {
 	/* 初始化 */
-  USARTx_Config ();                                                              //初始化串口1
+    USARTx_Config ();                                                              //初始化串口1
 	SysTick_Init ();                                                               //配置 SysTick 为 1ms 中断一次 
 	ESP8266_Init ();                                                               //初始化WiFi模块使用的接口和外设
-  LED_Init ();
-	
-	
-	printf ( "\r\n野火 WF-ESP8266 WiFi模块测试例程\r\n" );                          //打印测试例程提示信息
+    LED_Init ();
+
 	
 	
   ESP8266_StaTcpClient_UnvarnishTest ();
